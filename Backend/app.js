15c7c92105cbd1eb57e327848b834c2191a21953
Emilian Cismas <jackkan009@gmail.com>
2021-03-09 14:26:46 +0000

    removed middleware

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Sauce = require('./models/sauce');
+const sauceRoutes = require('./routes/sauce');
 
 const app = express();
 
@@ -27,104 +27,6 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/sauces', (req, res, next) => {
-    const sauce = new Sauce({
-        userId: req.body.userID,
-        name: req.body.name,
-        manufacturer: req.body.manufacturer,
-        description: req.body.description,
-        mainPepper: req.body.mainPepper,
-        imageUrl: url + '/images/' + req.file.filename,
-        heat: req.body.heat,
-        likes: 0,
-        dislikes: 0,
-        usersLiked: [],
-        usersDisliked: [],
-    });
-    sauce.save().then(
-        () => {
-            res.status(201).json({
-                message: 'Post saved successfully!'
-            })
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    )        
-  });
-
-app.get('api/sauces/:id', (req, res, next) => {
-    Sauce.findOne({
-        _id: req.params.id
-    }).then(
-        (sauce) => {
-            res.status(200).json(sauce);
-        }
-    ).catch(
-        (error) => {
-            res.status(404).json({
-                error: error
-            });
-        }
-    );
-});
-
-app.put('/api/sauces/:id', (req, res, next) => {
-    const sauce = new Sauce({
-        _id: req.params.id,
-        userId: req.body.userId,
-        name: req.body.name,
-        manufacturer: req.body.manufacturer,
-        description: req.body.description,
-        mainPepper: req.body.mainPepper,
-        heat: req.body.heat
-    });
-    Sauce.updateOne({ _id: req.params.id }, sauce).then(
-        () => {
-            res.status(201).json({
-                message: 'Sauce updated successfully!'
-            });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
-});
-
-app.use('/api/sauces', (req, res, next) => {
-    Sauce.find().then(
-        (sauces) => {
-            res.status(200).json(sauces);
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
-  });
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id }).then(
-        () => {
-            res.status(200).json({
-                message: 'Deleted!'
-            });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }      
-    );
-});
+app.use('/api/sauces', sauceRoutes);
 
 module.exports = app;
