7dc65f0390e733347444abe8ab70daad731831bd
Emilian Cismas <jackkan009@gmail.com>
2021-03-09 11:42:49 +0000

    added midlware to the app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,81 @@
+// MONGODB PASSWORD & USERNAME: 1B84nx3a76X5kWy5 Emillion777
+// MONGODB CONNECTION: mongodb+srv://Emillion777:1B84nx3a76X5kWy5@cluster0.j87p7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Sauce = require('./models/sauce');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://Emillion777:1B84nx3a76X5kWy5@cluster0.j87p7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.json());
+
+app.post('/api/sauces', (req, res, next) => {
+    const sauce = new Sauce({
+        userId: req.body.userID,
+        name: req.body.name,
+        manufacturer: req.body.manufacturer,
+        description: req.body.description,
+        mainPepper: req.body.mainPepper,
+        imageUrl: req.body.imageUrl,
+        heat: req.body.heat,
+        likes: req.body.likes,
+        dislikes: req.body.dislikes,
+        usersLiked: req.body.usersLiked,
+        usersDisliked: req.body.usersDisliked,
+    });
+    sauce.save().then(
+        () => {
+            res.status(201).json({
+                message: 'Post saved successfully!'
+            })
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    )        
+  });
+
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'My first thing',
+        description: 'All of the info about my first thing',
+        imageUrl: '',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'My second thing',
+        description: 'All of the info about my second thing',
+        imageUrl: '',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
 
 module.exports = app;
