a57831ab9f9cb550392bdca16df6463e58161fa6
Emilian Cismas <jackkan009@gmail.com>
2021-03-10 17:36:09 +0000

    delete sauce function added in controllers

--- Backend/controllers/sauce.js
+++ Backend/controllers/sauce.js
@@ -1,4 +1,5 @@
 const Sauce = require('../models/sauce');
+const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
     req.body.sauce = JSON.parse(req.body.sauce);
@@ -90,18 +91,25 @@ exports.modifySauce = (req, res, next) => {
 };
 
 exports.deleteSauce = (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id }).then(
-        () => {
-            res.status(200).json({
-                message: 'Deleted!'
+    Sauce.findOne({_id: req.params.id}).then(
+        (sauce) => {
+            const filename = thing.imageUrl.split('/images/')[1];
+            fs.unlink('images/' + filename, () => {
+                Sauce.deleteOne({ _id: req.params.id }).then(
+                    () => {
+                        res.status(200).json({
+                            message: 'Deleted!'
+                        });
+                    }
+                ).catch(
+                    (error) => {
+                        res.status(400).json({
+                            error: error
+                        });
+                    }      
+                );
             });
         }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }      
     );
 };
 
