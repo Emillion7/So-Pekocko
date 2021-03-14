eca7e1379051d4a961725be1446d0f25ef48f635
Emilian Cismas <jackkan009@gmail.com>
2021-03-14 11:54:22 +0000

    password requirements added

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,30 +1,26 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const userValidator = require('./user.validator.js');
 
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10).then(
-        (hash) => {
-            const user = new User({
-                email: req.body.email,
-                password: hash
-            });
-            user.save().then(
-                () => {
-                    res.status(201).json({
-                        message: 'User added successfully!'
-                    });
-                }
-            ).catch(
-                (error) => {
-                    res.status(500).json({
-                        error: error
-                    });
-                }
-            );
-        }
-    );
-};
+    if (userValidator.isGoodPassword(req.body.password)) {
+      bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+          const user = new User({
+            email: req.body.email,
+            password: hash
+          });
+          user.save()
+            .then(() => res.status(201).json({ message: 'User added successfully!' }))
+            .catch(error => res.status(400).json({ message: 'User already exist!' }));
+        })
+        .catch(error => res.status(500).json({ error }));
+    }
+    else {
+      return res.status(404).json({ message: 'The password must contain at least one number, one lower case, one upper case and must be at least 6 characters long!' });
+    }
+  };
 
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email }).then(
