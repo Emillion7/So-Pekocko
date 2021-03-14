fb0ad079884b8ad83c6658fad2029c121675a830
Emilian Cismas <jackkan009@gmail.com>
2021-03-14 13:06:12 +0000

    password requirments added

--- Backend/controllers/user.validator.js
+++ Backend/controllers/user.validator.js
@@ -1,6 +1,6 @@
 module.exports = {
     isGoodPassword: function (input) {
-        var regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+        let regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
         return regex.test(input);
     }
 }
